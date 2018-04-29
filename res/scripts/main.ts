9b7e6010754a45c18e39304096fec718a16dd786
Tushar <tusharshuklaa@gmail.com>
2018-04-29 17:31:30 +0530

    added disable all fxn and few other tweaks

--- res/scripts/main.ts
+++ res/scripts/main.ts
@@ -1,37 +1,115 @@
 namespace AlwaysOnWcm {
-    interface StringValidator { }
-  
-    export class WCMMode {
-      private wcmModeKey = "always_on_wcm";
-      private chrome: any = this.chrome || {};
-  
-      constructor() {}
-  
-      get mode():string {
-        return this.chrome.storage.sync.get([this.wcmModeKey], function(res) {
-          return res[this.wcmModeKey];
+  type ModeType = "disabled" | "edit" | "design" | "none";
+
+  interface AowcmElems {
+    switch?: string;
+    disabled?: string;
+    edit?: string;
+    design?: string;
+  }
+
+  export class WCMMode {
+    private wcmModeKey = "always_on_wcm";
+    private chrome: any = this.chrome || {};
+
+    constructor() {}
+
+    wcmmode: null;
+
+    get mode(): ModeType {
+      if (!this.wcmmode) {
+        return this.chrome.storage.sync.get([this.wcmModeKey], function (res) {
+          this.wcmmode = res[this.wcmModeKey];
+          console.log(`Value is "${ this.wcmmode }"`);
+          return this.wcmmode;
         });
-      }
-  
-      set mode(name: string) {
-        this.chrome.storage.sync.set({wcmModeKey: name}, function() {
-          console.log('Value is set to ' + name);
-        });
+      } else {
+        return this.wcmmode;
       }
     }
 
-    class Popup {
-        private wcmMode = new AlwaysOnWcm.WCMMode();
+    set mode(name: ModeType) {
+      this.chrome.storage.sync.set({
+        wcmModeKey: name
+      }, function () {
+        this.wcmmode = name;
+        console.log(`Value is set to "${ this.wcmmode }"`);
+      });
+    }
+  }
 
-        constructor() {}
+  export class Utils {
+    constructor() {}
 
-        private init() {
-            let self = this;
-            let mode = self.wcmMode.mode;
-            if(!mode) {
-                
-            }
+    static $(el: string): Element {
+      return document.querySelector(el);
+    }
+
+    static UpdateQueryString(key, value, url) {
+      if (!url) {
+        url = window.location.href;
+      }
+
+      const re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi");
+      let hash;
+
+      if (re.test(url)) {
+        if (typeof value !== 'undefined' && value !== null)
+          return url.replace(re, '$1' + key + "=" + value + '$2$3');
+        else {
+          hash = url.split('#');
+          url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+          if (typeof hash[1] !== 'undefined' && hash[1] !== null)
+            url += '#' + hash[1];
+          return url;
         }
+      } else {
+        if (typeof value !== 'undefined' && value !== null) {
+          var separator = url.indexOf('?') !== -1 ? '&' : '?';
+          hash = url.split('#');
+          url = hash[0] + separator + key + '=' + value;
+          if (typeof hash[1] !== 'undefined' && hash[1] !== null)
+            url += '#' + hash[1];
+          return url;
+        } else
+          return url;
+      }
     }
-}
+  }
 
+  class AowcmPopup {
+    private wcmMode = new AlwaysOnWcm.WCMMode();
+    private elems: AowcmElems = {
+      switch: "aowcm-always-on",
+      disabled: "aowcm-always-disabled",
+      edit: "aowcm-always-edit",
+      design: "aowcm-always-design"
+    };
+
+    constructor() {
+      let self = this;
+      window.onload = function () {
+        self.init();
+      };
+    }
+
+    public init() {
+      let self = this;
+      let $ = Utils.$;
+      if (!self.wcmMode.mode) {
+        self.wcmMode.mode = "none";
+        
+      }
+    }
+
+    private _disableAll(): void {
+      let self = this;
+      let $ = Utils.$;
+      Object.keys(self.elems).forEach((key,index) => {
+          if(key !== "switch") {
+            $(`#${self.elems[key]}`).setAttribute("disabled", "true");
+          }
+      });
+    }
+  }
+}
